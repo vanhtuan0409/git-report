54975303115907526ac5b8ff554edd41dca36b25
Tuấn Vương <vanhtuan0409@gmail.com>
2018-08-15 09:56:40 +0700

    init simple commit log

--- cmd/report/main.go
+++ cmd/report/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+
+	git "gopkg.in/src-d/go-git.v4"
+)
+
+const (
+	RepoPath = "/Users/tuanvuong/Workspace/nordotstrictrss/nordot-feedreader-fvc"
 )
 
 func main() {
-	fmt.Println("hello")
+	repo, err := git.PlainOpen(RepoPath)
+	if err != nil {
+		panic(err)
+	}
+
+	iter, err := repo.Log(&git.LogOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		commit, err := iter.Next()
+		if err != nil {
+			break
+		}
+		msg := fmt.Sprintf("%s", commit.String())
+		fmt.Println(msg)
+	}
 }
