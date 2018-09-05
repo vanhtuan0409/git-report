d323e51f7d9e2b50f4e74070e0e4cdb0c6248435
Tuấn Vương <vanhtuan0409@gmail.com>
2018-09-05 22:58:47 +0700

    add cli parser and refactor handler

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	GOOS=darwin GOARCH=amd64 go build -o bin/greport github.com/vanhtuan0409/git-report/cmd/greport
--- cmd/greport/main.go
+++ cmd/greport/main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
-	gitreport "github.com/vanhtuan0409/git-report"
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	configPath := gitreport.GetDefaultConfigPath()
-	config, err := gitreport.ReadConfigFromFile(configPath)
-	if err != nil {
-		panic(err)
-	}
+	app := cli.NewApp()
+	app.Name = "Git Report"
+	app.Usage = "Collect git commit messages and organize by days to create a daily report"
+	app.Version = "v0.1.0"
+	app.Action = generateReport
 
-	resultChan := make(chan string)
-	errChan := make(chan error)
-	for _, repoPath := range config.Repos {
-		go func(path string) {
-			gitClient := gitreport.NewGitClient(path)
-			result, err := gitClient.Log(&gitreport.LogOption{
-				Authors:           config.FilterEmail,
-				Limit:             5,
-				FetchAllBranch:    true,
-				FilterMergeCommit: true,
-			})
-			if err != nil {
-				errChan <- fmt.Errorf("Cannot fetch git commits from url: %s. Original Error:\n%s", path, err.Error())
-				return
-			}
-
-			generator := gitreport.NewReportGenerator()
-			report := generator.GenerateFromCommits(result)
-			resultChan <- report
-		}(repoPath)
-	}
-
-	for i := 0; i < len(config.Repos); i++ {
-		select {
-		case result := <-resultChan:
-			fmt.Println(result)
-		case err := <-errChan:
-			fmt.Println(err.Error())
-		}
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
 	}
 }
