682b36df594a9fe72c2e2760ac654546e390bf07
Tuấn Vương <vanhtuan0409@gmail.com>
2018-09-05 22:21:30 +0700

    improve report format

--- report.go
+++ report.go
@@ -27,8 +27,35 @@ func (r *reportGenerator) GenerateFromResults(results []*Result) string {
 func (r *reportGenerator) GenerateFromCommits(result *Result) string {
 	sb := new(strings.Builder)
 	fmt.Fprintf(sb, "Repository: %s\n", result.Repo)
-	for _, commit := range result.Commits {
-		fmt.Fprintf(sb, "    - %s: %s\n", "time", commit.Message())
+	groups := groupByDay(result.Commits)
+	for _, g := range groups {
+		fmt.Fprintf(sb, "  + %s\n", g.name)
+		for _, commit := range g.commits {
+			fmt.Fprintf(sb, "    - %s\n", commit.Message())
+		}
 	}
+
 	return sb.String()
+}
+
+type group struct {
+	name    string
+	commits []*GitCommit
+}
+
+func groupByDay(commits []*GitCommit) []*group {
+	groups := []*group{}
+	for _, commit := range commits {
+		dayStr := commit.Author.Date.Format("2006-01-02")
+		if len(groups) == 0 || groups[len(groups)-1].name != dayStr {
+			groups = append(groups, &group{
+				name:    dayStr,
+				commits: []*GitCommit{commit},
+			})
+		} else {
+			lastGroup := groups[len(groups)-1]
+			lastGroup.commits = append(lastGroup.commits, commit)
+		}
+	}
+	return groups
 }
